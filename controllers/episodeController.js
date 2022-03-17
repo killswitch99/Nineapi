47d89267f5ba99d80171b1d260096aa6a88b5055
Tofayel Ahmed Topu <53596291+killswitch99@users.noreply.github.com>
2022-03-17 23:36:31 +1100

    changes

--- controllers/episodeController.js
+++ controllers/episodeController.js
@@ -1,23 +1,7 @@
 //@desc  return image , slug and title
 //@route get request
 export const fomatItem = (req, res) => {
-	const isJson = (item) => {
-		item = typeof item !== 'string' ? JSON.stringify(item) : item
-		try {
-			item = JSON.parse(item)
-		} catch (e) {
-			return false
-		}
-		if (typeof item === 'object' && item !== null) {
-			return true
-		}
-		return false
-	}
-	if (
-		isJson(req.body) &&
-		req.body.hasOwnProperty('payload') &&
-		req.body.payload.length
-	) {
+	if (req.body.hasOwnProperty('payload') && req.body.payload.length) {
 		const response = req.body.payload
 			.filter((e) => e.drm != 'false' && e.episodeCount > 0)
 			.map(({ image, slug, title }) => {
