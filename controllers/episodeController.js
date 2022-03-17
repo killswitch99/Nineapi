ec4b4a2a4a3565964152d2a0f501be55af0606d2
Tofayel Ahmed Topu <53596291+killswitch99@users.noreply.github.com>
2022-03-18 00:04:25 +1100

    changes

--- controllers/episodeController.js
+++ controllers/episodeController.js
@@ -1,20 +1,32 @@
 //@desc  return image , slug and title
 //@route get request
 export const fomatItem = (req, res) => {
-	if (req.body.hasOwnProperty('payload') && req.body.payload.length) {
+	console.log(req)
+	const isJson = (item) => {
+		item = typeof item !== 'string' ? JSON.stringify(item) : item
+		try {
+			item = JSON.parse(item)
+		} catch (e) {
+			return false
+		}
+		if (typeof item === 'object' && item !== null) {
+			return true
+		}
+		return false
+	}
+	if (
+		isJson(req.body) &&
+		req.body.hasOwnProperty('payload') &&
+		req.body.payload.length
+	) {
 		const response = req.body.payload
 			.filter((e) => e.drm != 'false' && e.episodeCount > 0)
 			.map(({ image, slug, title }) => {
 				return { image: image.showImage, slug, title }
 			})
-		res
-			.status(200)
-			.set({
-				'Content-Type': 'application/json',
-			})
-			.json({ response: response })
+		res.status(200).contentType('application/json').json({ response: response })
 	} else {
-		res.status(400).json({
+		res.status(400).contentType('application/json').json({
 			error: 'Could not decode request: JSON parsing failed',
 		})
 	}
